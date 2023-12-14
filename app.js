d18ccfb823a700dde5cae43b917d2076fd99effb
mifshs <153731164+mifshs@users.noreply.github.com>
2023-12-14 21:37:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let admin, name;
+
+hello = "Джон";
+
+admin = hello;
+
+console.log( admin );
+
+let currentUserName = "Джон"
+
+
+
+
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+
+week.push("Восьмойдень")
+
+week.pop()
+
+
+console.log(week)
+
+
+let fruits = ["Киви", "Яблоко", "Банан", "Абрикос", "Апельсин"]
+
+fruits.splice(2,1, "киви")
+
+console.log(fruits)
+
+
+let imena = ["Иван", "Олег", "Катя", "Михаил", "Максим"]
+
+ if (imena.includes("Иван"))  {
+
+imena.push("Анна")
+
+console.log(imena) }
+
+else {
+    console.log("Имени нет")
+}
+
+
+
+
+
+
